901ba4a1e8171f001f701898c05e385bfabf5428
osman.setuval <osman.setuval@phoebustecnologia.com.br>
2022-10-11 10:16:42 -0300

    Updates

---
+++ static/js/7.7cf19111.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptapstore=this.webpackJsonptapstore||[]).push([[7],{67:function(t,s,a){"use strict";a.r(s),s.default=a.p+"static/media/RELEASENOTES.babfd1dc.md"}}]);
+//# sourceMappingURL=7.7cf19111.chunk.js.map
---
+++ static/media/RELEASENOTES.babfd1dc.md
@@ -0,0 +1,5 @@
+### 1.0.0
+Versão Inicial
+
+### 1.0.1
+Correções na release
